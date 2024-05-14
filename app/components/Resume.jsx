894625d0a43c85278d14d5f21fc7e4cf07e2cfcf
Daniel Antonio Tala de Dompierre de Chaufepie <daniel.talaman@gmail.com>
2024-05-14 21:38:57 +0200

    chore: Add newspaper layout to resume page

--- app/components/Resume.jsx
+++ app/components/Resume.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import ReactToPrint from "react-to-print";
 import "./resume.css";
+import me from '../assets/data/me.json';
 
 const data = [
   {
@@ -16,7 +17,13 @@ class ComponentToPrint extends React.Component {
     return (
       <div className="book">
         <div className="page">
-          <div className="subpage">Paage 1/2</div>
+          <div className="subpage">
+            <h1>Resume</h1>
+            <h2>{me.name}</h2>
+            <div class="newspaper">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.
+            </div>
+          </div>
         </div>
         <div className="page">
           <div className="subpage">Page 2/2</div>

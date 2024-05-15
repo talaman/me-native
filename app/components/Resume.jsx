e63a42fcc5a225016d01b766e575075e0d1be052
Daniel Antonio Tala de Dompierre de Chaufepie <daniel.talaman@gmail.com>
2024-05-15 12:37:34 +0200

    chore: Update resume layout and styling for better readability

--- app/components/Resume.jsx
+++ app/components/Resume.jsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import ReactToPrint from "react-to-print";
 import "./resume.css";
 import me from '../assets/data/me.json';
+import { Image } from 'react-native';
 
 const data = [
   {
@@ -20,26 +21,84 @@ class ComponentToPrint extends React.Component {
           <div className="subpage">
             <h1 className="h1-name">{me.name}</h1>
             <div className="info-row">
-                <span style={{paddingRight:180}}>{me.title}.</span>
-                <span>Phone: {me.contact.phone}</span>
-                <span>Web: {me.contact.website}</span>
+              <span style={{ paddingRight: 150 }}>{me.title}.</span>
+              <span>Phone: {me.contact.phone}</span>
+              <span>Web: {me.contact.website}</span>
             </div>
-            
+
             <div className="info-row">
-                <span>{me.age} years old, currently based in {me.location}.</span>
-                <span>Email: {me.contact.phone}</span>
+              <span>{me.age} years old, currently based in {me.location}.</span>
+              <span>LinkedIn: {me.contact.linkedin}</span>
+              <span>Email: {me.contact.phone}</span>
             </div>
             <div className="newspaper">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu 
+              <img src='../assets/images/photo.jpg' alt="My Image" />
+
+              <h2>Personal Statement</h2>
+              <p>
+                {me.personal_statement}
+              </p>
+              <h2>Personal Interests</h2>
+              <p>
+                {me.personal_interests}
+              </p>
+              <h2>Skills and Experience</h2>
+              <p>
+                {me.skills_and_experience}
+              </p>
+              <h2>Stack of Interest</h2>
+              <p>
+                {me.stack_of_interest}
+              </p>
+              <p>
+                I'm skilled in {me.skills}
+              </p>
+              <div>
+                <h2>Education</h2>
+                <ul>
+                  {me.education.map((edu, index) => (
+                    <li key={index}>
+                      {edu.degree}, {edu.institution}, {edu.year}.
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2>Relevant Work History</h2>
+                <ul>
+                  {me.work_history.map((job, index) => (
+                    <li key={index}>
+                      {job.title}, {job.company}, {job.startDate}-{job.endDate}
+                      <br />
+                      {job.description}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2>Languages</h2>
+                <ul>
+                  {me.languages.map((language, index) => (
+                    <li key={index}>
+                      {language.language}, {language.level}.
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2>References</h2>
+                <ul>
+                  {me.references.map((reference, index) => (
+                    <li key={index}>
+                      {reference.name}, {reference.title}, {reference.company}
+                      <br />
+                      LinkedIn: {reference.linkedin}
+                      <br />
+                      Phone: {reference.phone}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>

c58a5d2c0d9d3073300a74afa4c660dfeca5eb70
Daniel Antonio Tala de Dompierre de Chaufepie <daniel.talaman@gmail.com>
2024-05-27 10:08:57 +0200

    chore: Remove unused FontAwesome import in _layout.tsx

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,4 +1,3 @@
-// import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -22,7 +21,6 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    // ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
